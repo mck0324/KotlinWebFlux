af3d0be153e09b90b649d1e7530a9a6e20474622
mck0324 <dch3030@naver.com>
2024-02-18 01:10:57 +0900

    first commit(Spring WebFlux)

---
+++ src/main/kotlin/com/example/webfluxreactor/controller/HelloController.kt
@@ -0,0 +1,20 @@
+package com.example.webfluxreactor.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun hello(@RequestParam name: String): Mono<String> {
+        return Mono.just("Hello World $name ~")
+    }
+
+    @GetMapping("/")
+    fun index(): String {
+        return "main"
+    }
+}
---
+++ src/main/kotlin/com/example/webfluxreactor/model/Article.kt
@@ -0,0 +1,39 @@
+package com.example.webfluxreactor.model
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+import java.time.LocalDateTime
+
+@Table("TB_ARTICLE")
+class Article (
+    @Id
+    var id: Long = 0,
+    var title: String,
+    var body: String,
+    var authorId: Long
+): BaseEntity() {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Article
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int = id.hashCode()
+
+    override fun toString(): String =
+        "Article(id=$id, title='$title', body='$body', authorId=$authorId, ${super.toString()})"
+
+}
+open class BaseEntity (
+    var createdAt: LocalDateTime? = null,
+    var updatedAt: LocalDateTime? = null,
+) {
+    override fun toString(): String {
+        return "createdAt=$createdAt, updatedAt=$updatedAt"
+    }
+}
+
